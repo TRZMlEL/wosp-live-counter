6c7d8ee698238a5b4e651d75084ca8ba469f98b6
TRZMlEL <mateusztrzmiel2104@gmail.com>
2024-01-22 19:29:56 +0100

    java script for user

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="font-Scansky w-screen h-screen bg-bgWOSP bg-cover flex flex-col overflow-hidden items-center justify-center text-white gap-5">
-    <h1>
-      Hello user
-    </h1>
     <input v-model="amount" class="text-black" type="number" />
+    <div class="grid grid-cols-2 grid-rows-3 gap-10">
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add1">+1zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add2">+2zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add5">+5zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add10">+10zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add20">+20zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add50">+20zł</button>
+    </div>
     <button @click="send">Send</button>
   </div>
 </template>
@@ -19,6 +24,30 @@ socket.on('connect', () => {
     // Zaloguj się jako użytkownik
     socket.emit('login', 'user');
 });
+
+const add1 = () => {
+    amount.value += 1;
+}
+
+const add2 = () => {
+    amount.value += 2;
+}
+
+const add5 = () => {
+    amount.value += 5;
+}
+
+const add10 = () => {
+    amount.value += 10;
+}
+
+const add20 = () => {
+    amount.value += 20;
+}
+
+const add50 = () => {
+    amount.value += 50;
+}
 
 const send = () => {
     socket.emit('transaction', amount.value);
