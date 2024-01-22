96e7472b434df430174f49f99b0f56fa44d524a6
TRZMlEL <mateusztrzmiel2104@gmail.com>
2024-01-22 20:10:21 +0100

    user styles

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="font-Scansky w-screen h-screen bg-bgWOSP bg-cover flex flex-col overflow-hidden items-center justify-center text-white gap-5">
-    <input v-model="amount" class="text-black" type="number" />
-    <div class="grid grid-cols-2 grid-rows-3 gap-10">
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add1">+1zł</button>
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add2">+2zł</button>
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add5">+5zł</button>
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add10">+10zł</button>
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add20">+20zł</button>
-      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg" @click="add50">+20zł</button>
+  <div class="font-Scansky w-screen h-screen bg-bgWOSP bg-cover flex flex-col overflow-hidden items-center justify-center text-white">
+    <div class="p-1 bg-white rounded-lg w-5/6 sm:w-4/6 md:w-2/5 lg:w-1/5 mt-5">
+      <input v-model="amount" class="text-black p-2.5 text-4xl rounded-lg border-8 border-black border-solid w-full" placeholder="0" type="number" />
     </div>
-    <button @click="send">Send</button>
+    <div class="grid grid-cols-2 grid-rows-3 gap-5 m-5">
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add1">+1zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add2">+2zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add5">+5zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add10">+10zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add20">+20zł</button>
+      <button class="bg-white text-black text-2xl flex items-center justify-center p-10 rounded-lg border-4 border-black border-solid active:bg-gray-300" @click="add50">+50zł</button>
+    </div>
+    <button  class="bg-white text-black text-4xl flex items-center justify-center p-5 mb-5 w-5/6 sm:w-4/6 md:w-2/5 lg:w-1/5 rounded-lg border-4 border-black border-solid active:bg-gray-300"  @click="send">Wpłać!</button>
   </div>
 </template>
 
@@ -50,6 +52,9 @@ const add50 = () => {
 }
 
 const send = () => {
+    amount.value = parseInt(amount.value);
     socket.emit('transaction', amount.value);
+    amount.value = 0;
 }
+
 </script>
