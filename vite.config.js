2246afd51ec3374995f6fbcff41c151ce1a4433b
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-02-04 22:29:10 +0100

    fix: 🚀 GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/wosp-live-counter/', // Zastąp <nazwa-repozytorium> nazwą swojego repozytorium
+  base: process.env.NODE_ENV === 'production' ? '/wosp-live-counter/' : '/',
 })
